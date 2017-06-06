318c51a9d392458dbd41b35e63ed6869912ea1a9
Memoona Mughal <memoona.mughal@grandcentrix.net>
2017-06-06 17:44:34 +0200

    Added dependencies for absinthe and faker

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule GraphQL.Mixfile do
   def application do
     [mod: {GraphQL, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :faker]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +37,12 @@ defmodule GraphQL.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:absinthe, "~> 1.3.0"},
+     {:absinthe_plug, "~> 1.1"},
+     {:absinthe_ecto, git: "https://github.com/absinthe-graphql/absinthe_ecto.git"},
+     {:poison, "~> 2.0"},
+     {:faker, "~> 0.7"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
